9d4cae94ec06a61ca5bcb424841d4a721c03247c
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2023-03-01 11:08:25 -0300

    conditionals

--- conditionals.sh
+++ conditionals.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+echo "Welcome! Select your class: 
+1 - Samurai
+2 - Prisioner
+3 - Barbarian
+4 - Stupid stick man"
+
+read class
+
+case $class in
+
+    1)
+        type="Samurai"
+        hp=10
+        power=20
+        ;;
+    2)  
+        type="Prisioner"
+        hp=30
+        power=5
+        ;;
+    3)
+        type="Barbarian"
+        hp=20
+        power=30
+        ;;
+    4) 
+        type="Stupid stick man"
+        hp=1
+        power=1
+        ;;
+esac
+
+echo "Your class is $type. HP: $hp. POWER: $power"
+if [[ $type == "Stupid stick man" ]]; then
+    echo "You must be really stupid"
+fi
+
+sleep 2
+
 echo "Do you like coffee? (y/n)"
 
 read coffee
